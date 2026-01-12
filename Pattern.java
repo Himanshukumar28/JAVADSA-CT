50c25785b9789762d62f805c79b9c934a5c42fce
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-01-12 19:55:26 +0530

    reverse

--- Pattern.java
+++ Pattern.java
@@ -50,11 +50,25 @@ public class Pattern {
         //     System.out.println();
         // }
 
-        for(int i = 1; i<=4; i++){
-            for(int j =3; j>=i; j--){
+        //STAR WITH SPACE
+
+        // for(int i = 1; i<=4; i++){
+        //     for(int j =3; j>=i; j--){
+        //         System.out.print(" ");
+        //     }
+        //     for(int k=1; k<=i; k++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        //REVERSE WITH SPACE
+
+        for(int i = 1; i<=4;i++){
+            for(int j=2;j<=i;j++){
                 System.out.print(" ");
             }
-            for(int k=1; k<=i; k++){
+            for(int k=4; k>=i; k--){
                 System.out.print("*");
             }
             System.out.println();
