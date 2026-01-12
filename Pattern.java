7bea2d6f23738f50e196d582db9de99162065c1e
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-01-12 20:11:17 +0530

    pyramid_Q

--- Pattern.java
+++ Pattern.java
@@ -57,18 +57,29 @@ public class Pattern {
         //         System.out.print(" ");
         //     }
         //     for(int k=1; k<=i; k++){
-        //         System.out.print("*");
+        //         System.out.print("* ");
         //     }
         //     System.out.println();
         // }
 
         //REVERSE WITH SPACE
 
-        for(int i = 1; i<=4;i++){
-            for(int j=2;j<=i;j++){
+        // for(int i = 1; i<=4;i++){
+        //     for(int j=2;j<=i;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int k=4; k>=i; k--){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+         //PYRAMID
+        for(int i = 1; i<=4; i++){
+            for(int j =3; j>=i;j--){
                 System.out.print(" ");
             }
-            for(int k=4; k>=i; k--){
+            for(int k= 1;k<=(2*i-1);k++){
                 System.out.print("*");
             }
             System.out.println();
