1dc7721afd0e1fe6238963163220e110168f7b1d
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-01-12 19:47:40 +0530

    pattern

--- Pattern.java
+++ Pattern.java
@@ -24,12 +24,12 @@ public class Pattern {
         // }
 
         // OR
-        for(int i=1; i<=5; i++){
-            for(int j=4; j>=i; j--){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+        // for(int i=1; i<=5; i++){
+        //     for(int j=4; j>=i; j--){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
         
         
         // char ch = 'A';
@@ -49,6 +49,16 @@ public class Pattern {
         //     }
         //     System.out.println();
         // }
+
+        for(int i = 1; i<=4; i++){
+            for(int j =3; j>=i; j--){
+                System.out.print(" ");
+            }
+            for(int k=1; k<=i; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     
     }
 }
