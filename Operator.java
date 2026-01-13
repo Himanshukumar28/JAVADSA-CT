5b3b85a557b340e7463951860ace3fbc81b14584
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-01-13 23:18:04 +0530

    que

--- Operator.java
+++ Operator.java
@@ -1,10 +1,19 @@
 public class Operator {
     public static void main(String[] args) {
         //unary operator
-        int a = 5;
-        a++;
-        ++a;
-        a++;
-        System.out.println(a);
+        // int a = 5;
+        // a++;
+        // ++a;
+        // a++;
+        // System.out.println(a);
+
+        int a = 12;
+        int b = 23;
+
+        int c =  a++ + b-- + ++a + ++b - ++a;
+        System.out.println(c);
+
+
+
     }
 }
