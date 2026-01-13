0590f9f635db2af958bbf3e51c7108421445972e
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-01-13 23:15:45 +0530

    opt

---
+++ Operator.java
@@ -0,0 +1,10 @@
+public class Operator {
+    public static void main(String[] args) {
+        //unary operator
+        int a = 5;
+        a++;
+        ++a;
+        a++;
+        System.out.println(a);
+    }
+}
