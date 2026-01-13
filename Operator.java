0541416418be133a9eeeb94d3f09d601466c5539
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-01-13 23:21:36 +0530

    pre & post

--- Operator.java
+++ Operator.java
@@ -7,11 +7,21 @@ public class Operator {
         // a++;
         // System.out.println(a);
 
-        int a = 12;
-        int b = 23;
+        // int a = 12;
+        // int b = 23;
 
-        int c =  a++ + b-- + ++a + ++b - ++a;
-        System.out.println(c);
+        // int c =  a++ + b-- + ++a + ++b - ++a;
+        // System.out.println(c);
+
+            int a = 10;  
+ //  int b = ++a; //pre increment
+ //   int b = a++; //post increment
+
+ //   int b = --a; //pre decrement
+          int b = a--;  //post decrement
+            System.out.println(a);
+            System.out.println(b);
+    
 
 
 
