91d1ff164a694709a1ac0d59169df8a70231c811
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-01-14 23:21:00 +0530

    except

--- Oue_Operator.java
+++ Oue_Operator.java
@@ -5,12 +5,17 @@ public class Oue_Operator {
         // i = i++ +  ++i;
         // System.out.println(i);
 
-        int a = 11; 
-        int b = 22; 
-        int c ;
-        c = a + b  + a++ + b++  + a++  + b++;
-        System.out.println("a=" + a);
-        System.out.println("b="+ b);
-        System.out.println("c="+c);
+        // int a = 11; 
+        // int b = 22; 
+        // int c ;
+        // c = a + b  + a++ + b++  + a++  + b++;
+        // System.out.println("a=" + a);
+        // System.out.println("b="+ b);
+        // System.out.println("c="+c);
+
+        //compilation error:
+        int a = 11++;
+        System.out.println(a);
+        
     }
 }
