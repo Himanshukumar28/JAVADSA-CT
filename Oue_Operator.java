e1596a87adbaefdcd9176dfdcccfcc9ce48a4cb8
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-01-14 23:14:45 +0530

    Que

--- Oue_Operator.java
+++ Oue_Operator.java
@@ -1,9 +1,16 @@
 public class Oue_Operator {
     public static void main(String[] args) {
         //Que01
-        int i = 11;
+        // int i = 11;
+        // i = i++ +  ++i;
+        // System.out.println(i);
 
-        i = i++ +  ++i;
-        System.out.println(i);
+        int a = 11; 
+        int b = 22; 
+        int c ;
+        c = a + b  + a++ + b++  + a++  + b++;
+        System.out.println("a=" + a);
+        System.out.println("b="+ b);
+        System.out.println("c="+c);
     }
 }
