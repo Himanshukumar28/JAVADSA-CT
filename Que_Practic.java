4639ebd5e588d31d6c38d6f4570caf97afdbfe25
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-01-16 18:20:25 +0530

    tribonacci

--- Que_Practic.java
+++ Que_Practic.java
@@ -28,25 +28,42 @@ public class Que_Practic {
         // System.out.println(smax);
 
         //PRIME NUM
-        int n = sc.nextInt();
-        if(n<=1){
-            System.out.println("Neha is Sad");
+        // int n = sc.nextInt();
+        // if(n<=1){
+        //     System.out.println("Neha is Sad");
+        // }
+        // else{
+        //     boolean isprime = true;
+        //     for(int i =2; i*i<=n; i++){
+        //         if(n%i==0){
+        //             isprime  = false;
+        //             break;
+        //         }
+        //     }
+        //     if(isprime){
+        //         System.out.println("Neha is Happy");
+        //     }
+        //     else{
+        //         System.out.println("Neha is Sad");
+        //     }
+        // }
+
+        //TRIBONACCI SERIES
+        int  a = 0;
+        int b = 0;
+        int c = 1;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        for(int i=4; i<=10; i++){
+        int d = a + b + c ;
+            System.out.println(d+" ");
+            a = b;
+            b =c;
+            c =d;
+    
         }
-        else{
-            boolean isprime = true;
-            for(int i =2; i*i<=n; i++){
-                if(n%i==0){
-                    isprime  = false;
-                    break;
-                }
-            }
-            if(isprime){
-                System.out.println("Neha is Happy");
-            }
-            else{
-                System.out.println("Neha is Sad");
-            }
-        }
+    
 
 
     }
