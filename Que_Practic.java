18f9691fc8482a7889be7b6da41c13d6e42e4149
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-01-16 17:11:42 +0530

    prime

--- Que_Practic.java
+++ Que_Practic.java
@@ -2,28 +2,51 @@ import java.util.*;
 public class Que_Practic {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        int d = sc.nextInt();
 
-        int smax = 0;
-        int max  =Math.max(Math.max(Math.max(a,b),c),d);
-        int min = Math.min(Math.min(Math.min(a,b),c),d);
+        //SECOND MAXIMUM
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int c = sc.nextInt();
+        // int d = sc.nextInt();
 
-        if(a!=max && a!=min){
-            smax = Math.max(smax,a);
+        // int smax = 0;
+        // int max  =Math.max(Math.max(Math.max(a,b),c),d);
+        // int min = Math.min(Math.min(Math.min(a,b),c),d);
+
+        // if(a!=max && a!=min){
+        //     smax = Math.max(smax,a);
+        // }
+        // if(b!=max && b!=min){
+        //     smax = Math.max(smax,b);
+        // }
+        // if(c!=max && c!=min){
+        //     smax = Math.max(smax,c);
+        // }
+        // if(d!=max && d!=min){
+        //     smax = Math.max(smax,d);
+        // }
+        // System.out.println(smax);
+
+        //PRIME NUM
+        int n = sc.nextInt();
+        if(n<=1){
+            System.out.println("Neha is Sad");
         }
-        if(b!=max && b!=min){
-            smax = Math.max(smax,b);
+        else{
+            boolean isprime = true;
+            for(int i =2; i*i<=n; i++){
+                if(n%i==0){
+                    isprime  = false;
+                    break;
+                }
+            }
+            if(isprime){
+                System.out.println("Neha is Happy");
+            }
+            else{
+                System.out.println("Neha is Sad");
+            }
         }
-        if(c!=max && c!=min){
-            smax = Math.max(smax,c);
-        }
-        if(d!=max && d!=min){
-            smax = Math.max(smax,d);
-        }
-        System.out.println(smax);
 
 
     }
