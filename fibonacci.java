5850feb2607e8b2327514715fafb2036884c55a6
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-01-16 17:26:02 +0530

    fibonachi

--- fibonacci.java
+++ fibonacci.java
@@ -9,33 +9,33 @@ public class fibonacci {
         // System.out.println("Enter Max Value");
         // int val = sc.nextInt();
         // while (z<=val) {
-        //     System.out.println(z);
+        //     System.out.print(z);
         //     x = y;
         //     y = z;
         //     z = x + y;
         // }
 
         //IN TERMS USING WHILE LOOP
-    //     Scanner sc = new Scanner(System.in); 
-    //     int x = 0;
-    //     int y = 1;
-    //     int z =0;
-    //     int term = 1;
-    //     System.out.println("Enter value");
-    //     int N = sc.nextInt();
-    //     while(term<=N){
-    //         System.out.println(z);
-    //         x = y;
-    //         y = z ;
-    //         z =x+y;
-    //         term = term + 1;
-    //     }
+      //  Scanner sc = new Scanner(System.in); 
+        // int x = 0;
+        // int y = 1;
+        // int z =0;
+        // int term = 1;
+        // System.out.println("Enter value");
+        // int N = sc.nextInt();
+        // while(term<=N){
+        //     System.out.println(z);
+        //     x = y;
+        //     y = z ;
+        //     z =x+y;
+        //     term = term + 1;
+        // }
 
         //FIBONACCI USING FOR LOOP 
         int a = 0;
         int b = 1;
         int c  = 0;
-        for(int i = 1; i<=10; i++){
+        for(int i = 1; i<=12; i++){
             System.out.println(c);
             a = b;
             b = c;
