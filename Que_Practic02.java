a70f5876e4bfded167fcb62ef0c8e854cb965aca
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-01-21 21:36:16 +0530

    Double Rev

--- Que_Practic02.java
+++ Que_Practic02.java
@@ -12,16 +12,28 @@ public class Que_Practic02 {
         // System.out.println(sint);
 
         //LEETCODE 2427
-        int a = 12;
-        int b = 6;
-        int cout = 0;
-        for(int i = 1;i<=Math.min(a,b); i++){
-            if(a%i==0 && b%i==0){
-               // cout = cout + 1;
-                cout++;
-            }
+        // int a = 12;
+        // int b = 6;
+        // int cout = 0;
+        // for(int i = 1;i<=Math.min(a,b); i++){
+        //     if(a%i==0 && b%i==0){
+        //        // cout = cout + 1;
+        //         cout++;
+        //     }
+        // }
+        // System.out.println(cout);
+
+        //LEETCODE 2119
+        int num = 250;
+        if(num ==0){
+            System.out.println(true);
         }
-        System.out.println(cout);
+        else if(num % 10==0){
+            System.out.println(false);
+        }
+        else{
+            System.out.println(true);
+        }
 
     }
 }
