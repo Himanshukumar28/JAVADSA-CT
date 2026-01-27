978063fffdbb240758240f1a33a181b1080f6adb
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-01-27 23:20:57 +0530

    str

--- Pattern.java
+++ Pattern.java
@@ -52,27 +52,27 @@ public class Pattern {
 
         //STAR WITH SPACE
 
-        // for(int i = 1; i<=4; i++){
-        //     for(int j =3; j>=i; j--){
-        //         System.out.print(" ");
-        //     }
-        //     for(int k=1; k<=i; k++){
-        //         System.out.print("* ");
-        //     }
-        //     System.out.println();
-        // }
-
-        //REVERSE WITH SPACE
-
-        for(int i = 1; i<=4;i++){
-            for(int j=2;j<=i;j++){
+        for(int i = 1; i<=4; i++){
+            for(int j =3; j>=i; j--){
                 System.out.print(" ");
             }
-            for(int k=4; k>=i; k--){
-                System.out.print("$");
+            for(int k=1; k<=i; k++){
+                System.out.print("&");
             }
             System.out.println();
         }
+
+        //REVERSE WITH SPACE
+
+        // for(int i = 1; i<=4;i++){
+        //     for(int j=2;j<=i;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int k=4; k>=i; k--){
+        //         System.out.print("$");
+        //     }
+        //     System.out.println();
+        // }
 
          //PYRAMID
         // for(int i = 1; i<=4; i++){
