ae80bf6a92ef1d0946784bf19c5929300f2df239
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-01-27 23:22:42 +0530

    ptr

--- Pattern.java
+++ Pattern.java
@@ -15,13 +15,13 @@ public class Pattern {
         //     System.out.println();
         // }
     
-        // int n = 5;
-        // for(int i= 1; i<=n; i++){
-        //     for(int j= 1; j<=n-i+1;j++){
-        //         System.out.print("*");
-        //     }
-        //     System.out.println();
-        // }
+        int n = 5;
+        for(int i= 1; i<=n; i++){
+            for(int j= 1; j<=n-i+1;j++){
+                System.out.print("@");
+            }
+            System.out.println();
+        }
 
         // OR
         // for(int i=1; i<=5; i++){
@@ -52,15 +52,15 @@ public class Pattern {
 
         //STAR WITH SPACE
 
-        for(int i = 1; i<=4; i++){
-            for(int j =3; j>=i; j--){
-                System.out.print(" ");
-            }
-            for(int k=1; k<=i; k++){
-                System.out.print("&");
-            }
-            System.out.println();
-        }
+        // for(int i = 1; i<=4; i++){
+        //     for(int j =3; j>=i; j--){
+        //         System.out.print(" ");
+        //     }
+        //     for(int k=1; k<=i; k++){
+        //         System.out.print("&");
+        //     }
+        //     System.out.println();
+        // }
 
         //REVERSE WITH SPACE
 
