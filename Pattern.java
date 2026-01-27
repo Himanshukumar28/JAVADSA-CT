4045c58b55c7515a7903c7cd80bfc97a33d38b7d
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-01-27 23:20:11 +0530

    spc

--- Pattern.java
+++ Pattern.java
@@ -64,26 +64,26 @@ public class Pattern {
 
         //REVERSE WITH SPACE
 
-        // for(int i = 1; i<=4;i++){
-        //     for(int j=2;j<=i;j++){
-        //         System.out.print(" ");
-        //     }
-        //     for(int k=4; k>=i; k--){
-        //         System.out.print("*");
-        //     }
-        //     System.out.println();
-        // }
-
-         //PYRAMID
-        for(int i = 1; i<=4; i++){
-            for(int j =3; j>=i;j--){
+        for(int i = 1; i<=4;i++){
+            for(int j=2;j<=i;j++){
                 System.out.print(" ");
             }
-            for(int k= 1;k<=(2*i-1);k++){
-                System.out.print("#");
+            for(int k=4; k>=i; k--){
+                System.out.print("$");
             }
             System.out.println();
         }
+
+         //PYRAMID
+        // for(int i = 1; i<=4; i++){
+        //     for(int j =3; j>=i;j--){
+        //         System.out.print(" ");
+        //     }
+        //     for(int k= 1;k<=(2*i-1);k++){
+        //         System.out.print("#");
+        //     }
+        //     System.out.println();
+        // }
 
         
     
