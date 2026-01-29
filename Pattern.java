319a9b799a55046dbcececd6abd6f55e2585ec5f
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-01-29 17:22:59 +0530

    bh

--- Pattern.java
+++ Pattern.java
@@ -1,19 +1,20 @@
 public class Pattern {
     public static void main(String[] args) {
 
-        // for(int i = 1; i<=5; i++){
-        //     for(int j=1;j<=5; j++){
-        //         System.out.print("*");
-        //     }
-        // System.out.println();
-        // }
-
-        for(int i=1; i<=5; i++){
-            for(int j = 1; j<=i; j++){
+        int n = 6;
+        for(int i = 1; i<=n; i++){
+            for(int j=1;j<=5; j++){
                 System.out.print("#");
             }
-            System.out.println();
+        System.out.println();
         }
+
+        // for(int i=1; i<=5; i++){
+        //     for(int j = 1; j<=i; j++){
+        //         System.out.print("#");
+        //     }
+        //     System.out.println();
+        // }
     
         // int n = 5;
         // for(int i= 1; i<=n; i++){
