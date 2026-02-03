8ce78bbbbbcd94c92f66519e98a4c7a438f2ca3d
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-02-03 23:03:58 +0530

    nj

--- Pattern01.java
+++ Pattern01.java
@@ -23,9 +23,9 @@ public class Pattern01 {
 
         for(int i = 1; i<=n; i++){
             for(int j = 1; j<=n-1; j++){
-                System.out.print(" ");
+                System.out.print("#");
             }
-            for(int k = n; k<=i; k++){
+            for(int k = 1; k<=i; k++){
                 System.out.print("*");
             }
             System.out.println();
