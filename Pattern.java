f046da1b25247d1c51eda88c471212f1d536fc76
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-02-03 23:18:25 +0530

    *pat

--- Pattern.java
+++ Pattern.java
@@ -1,13 +1,13 @@
 public class Pattern {
     public static void main(String[] args) {
 
-        int n = 6;
-        for(int i = 1; i<=n; i++){
-            for(int j=1;j<=5; j++){
-                System.out.print("#");
-            }
-        System.out.println();
-        }
+        // int n = 6;
+        // for(int i = 1; i<=n; i++){
+        //     for(int j=1;j<=5; j++){
+        //         System.out.print("#");
+        //     }
+        // System.out.println();
+        // }
 
         // for(int i=1; i<=5; i++){
         //     for(int j = 1; j<=i; j++){
@@ -16,13 +16,13 @@ public class Pattern {
         //     System.out.println();
         // }
     
-        // int n = 5;
-        // for(int i= 1; i<=n; i++){
-        //     for(int j= 1; j<=n-i+1;j++){
-        //         System.out.print("@");
-        //     }
-        //     System.out.println();
-        // }
+        int n = 5;
+        for(int i= 1; i<=n; i++){
+            for(int j= 1; j<=n-i+1;j++){
+                System.out.print("@");
+            }
+            System.out.println();
+        }
 
         // OR
         // for(int i=1; i<=5; i++){
