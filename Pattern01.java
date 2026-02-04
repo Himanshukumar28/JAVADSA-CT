cfad9bf1bd256ded2e3dd4c9a9b9cf9ad6acbf76
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-02-04 22:53:12 +0530

    revpatt

--- Pattern01.java
+++ Pattern01.java
@@ -14,12 +14,12 @@ public class Pattern01 {
         //     System.out.println();
         // }
 
-            for(int i = 1; i<=n; i++){
-                for(int j = 1; j<=i; j++){
-                    System.out.print("*");
-                }
-                System.out.println();
-            }
+            // for(int i = 1; i<=n; i++){
+            //     for(int j = 1; j<=i; j++){
+            //         System.out.print("*");
+            //     }
+            //     System.out.println();
+            // }
 
         // for(int i = n; i>=1; i--){
         //     for(int j = 1; j<=i; j++){
@@ -28,15 +28,15 @@ public class Pattern01 {
         //     System.out.println();
         // }
 
-        // for(int i = 1; i<=n; i++){
-        //     for(int j = 1; j<=n-1; j++){
-        //         System.out.print("#");
-        //     }
-        //     for(int k = 1; k<=i; k++){
-        //         System.out.print("*");
-        //     }
-        //     System.out.println();
-        // }
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=n-i; j++){
+                System.out.print("#");
+            }
+            for(int k = 1; k<=i; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
         
 
