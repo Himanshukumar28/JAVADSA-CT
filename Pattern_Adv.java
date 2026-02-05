832b606486152fbd14570e8bc9d2016a5a7f88ed
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-02-05 23:32:00 +0530

    butterfly

--- Pattern_Adv.java
+++ Pattern_Adv.java
@@ -21,7 +21,22 @@ public class Pattern_Adv {
 
         //REVERS HALF BUTTER FLY
 
-        for(int i = 1; i<=n; i++){
+        // for(int i = n; i>=1; i--){
+        //     for(int j = 1; j<=i; j++){
+        //         System.out.print("*");
+        //     }
+        //     for(int j = 1; j<=2*(n-i); j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j = 1; j<=i; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        //BUTTER FLY
+
+        for(int i = 1; i<=n-1; i++){
             for(int j = 1; j<=i; j++){
                 System.out.print("*");
             }
@@ -45,6 +60,33 @@ public class Pattern_Adv {
             }
             System.out.println();
         }
+
+        //DIAMOND 
+
+        // for(int i = n; i>=1; i--){
+        //     for(int j = 1; j<=i; j++){
+        //         System.out.print("*");
+        //     }
+        //     for(int j = 1; j<=2*(n-i); j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j = 1; j<=i; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+        // for(int i = 1; i<=n; i++){
+        //     for(int j = 1; j<=i; j++){
+        //         System.out.print("*");
+        //     }
+        //     for(int j = 1; j<=2*(n-i); j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j = 1; j<=i; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
     
 
 
