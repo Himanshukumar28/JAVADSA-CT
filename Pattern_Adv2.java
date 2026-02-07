328f83496d5d5cc245309189de09e6db5fbbaae1
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-02-07 20:22:38 +0530

    num

--- Pattern_Adv2.java
+++ Pattern_Adv2.java
@@ -35,21 +35,35 @@ public class Pattern_Adv2 {
         //     System.out.println();
         // }
 
+        //DIAMOND
+        // for(int i = 1; i<=n; i++){
+        //     for(int j = 1; j<=n-i; j++){
+        //         System.out.print(" ");  //space
+        //     }
+        //     for(int j = 1; j<=2*i-1; j++){
+        //         System.out.print("*");  //star
+        //     }
+        //     System.out.println();
+        // }
+        // for(int i = n; i>=1; i--){
+        //     for(int j = 1; j<=n-i; j++){
+        //         System.out.print(" ");  //space
+        //     }
+        //     for(int j = 1; j<=2*i-1; j++){
+        //         System.out.print("*");  //star
+        //     }
+        //     System.out.println();
+        // }
+
         for(int i = 1; i<=n; i++){
-            for(int j = 1; j<=n-i; j++){
+            for(int j = 1; j<=i; j++){
+                System.out.print(i);  //num
+            }
+            for(int j = 1; j<=2*(n-i); j++){
                 System.out.print(" ");  //space
             }
-            for(int j = 1; j<=2*i-1; j++){
-                System.out.print("*");  //star
-            }
-            System.out.println();
-        }
-        for(int i = n; i>=1; i--){
-            for(int j = 1; j<=n-i; j++){
-                System.out.print(" ");  //space
-            }
-            for(int j = 1; j<=2*i-1; j++){
-                System.out.print("*");  //star
+            for(int j = 1; j<=i; j++){
+                System.out.print(i);  //num
             }
             System.out.println();
         }
