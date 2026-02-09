02be3b874a363f6c6e9b7d4fc327274fe9a20fd3
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-02-09 22:45:42 +0530

    tri

--- Pattern_Adv2.java
+++ Pattern_Adv2.java
@@ -25,15 +25,15 @@ public class Pattern_Adv2 {
 
         //TRIANGLE
 
-        // for(int i = 1; i<=n; i++){
-        //     for(int j = 1; j<=n-i; j++){
-        //         System.out.print(" ");  //space
-        //     }
-        //     for(int j = 1; j<=2*i-1; j++){
-        //         System.out.print("*");  //star
-        //     }
-        //     System.out.println();
-        // }
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=n-i; j++){
+                System.out.print(" ");  //space
+            }
+            for(int j = 1; j<=2*i-1; j++){
+                System.out.print("*");  //star
+            }
+            System.out.println();
+        }
 
         //DIAMOND
         // for(int i = 1; i<=n; i++){
