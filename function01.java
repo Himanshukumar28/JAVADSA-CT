9ee991193f243e65ecaef3b374aff56637f2f861
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-02-10 20:20:03 +0530

    tribo

--- function01.java
+++ function01.java
@@ -15,16 +15,16 @@ public class function01 {
         return sum;
     }
 
-    // public static int ttribonaci(int n){
-    //     if(n==0 || n==1) return n;
-    //     if(n==2) return 1;
-    //     int a = 0 , b = 1 , c = 1 , sum = 0;
-    //     for(int i = 3; i<=n; i++){
-    //         sum = a + b +c;
-    //         a = b;
-    //         b = c;
-    //         c = sum;
-    //     }
-    //     return sum;
-    // }
+    public static int ttribonaci(int n){
+        if(n==0 || n==1) return n;
+        if(n==2) return 1;
+        int a = 0 , b = 1 , c = 1 , sum = 0;
+        for(int i = 3; i<=n; i++){
+            sum = a + b +c;
+            a = b;
+            b = c;
+            c = sum;
+        }
+        return sum;
+    }
 }
