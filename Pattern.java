5d519b4dcbd7cb612f7f00babbc440df0b27c998
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-02-11 22:09:05 +0530

    final

--- Pattern.java
+++ Pattern.java
@@ -77,15 +77,15 @@ public class Pattern {
         // }
 
          //PYRAMID
-        // for(int i = 1; i<=4; i++){
-        //     for(int j =3; j>=i;j--){
-        //         System.out.print(" ");
-        //     }
-        //     for(int k= 1;k<=(2*i-1);k++){
-        //         System.out.print("#");
-        //     }
-        //     System.out.println();
-        // }
+        for(int i = 1; i<=4; i++){
+            for(int j =3; j>=i;j--){
+                System.out.print(" ");
+            }
+            for(int k= 1;k<=(2*i-1);k++){
+                System.out.print("#");
+            }
+            System.out.println();
+        }
 
         
     
