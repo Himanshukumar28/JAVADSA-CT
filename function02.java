fd2400264c9c1070a928831db8b7f8598f8c5a2d
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-02-12 01:02:17 +0530

    fun_ps

--- function02.java
+++ function02.java
@@ -3,6 +3,7 @@ public class function02 {
         int n = 1234;
         System.out.println(reverse(n));
         System.out.println(sum(n));
+        System.out.println(primesum(n));
     }
 
     public static int reverse(int n){
@@ -23,5 +24,17 @@ public class function02 {
             n = n/10; //    / 10 → removes last digit
         }
         return s;
-    } 
+    }
+    
+    public static int primesum(int n){
+        int ps = 0;
+        while(n!= 0){
+            int rem = n % 10;
+            if(rem ==2|| rem == 3 || rem == 5 || rem ==7){
+                ps = ps  + rem;
+            }
+            n = n/10;
+        }
+        return ps;
+    }
 }
