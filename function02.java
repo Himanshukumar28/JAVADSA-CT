b0b6d296e139b2c6be4093409df6513d939a9c39
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-02-12 00:50:22 +0530

    fun_sum

--- function02.java
+++ function02.java
@@ -2,6 +2,7 @@ public class function02 {
     public static void main(String[] args) {
         int n = 1234;
         System.out.println(reverse(n));
+        System.out.println(sum(n));
     }
 
     public static int reverse(int n){
@@ -14,5 +15,13 @@ public class function02 {
         return rev;
     }
 
-    public static 
+    public static int sum(int n){
+        int s = 0;
+        while(n != 0){
+            int rem  = n%10; //  % 10 → gets last digit
+            s = s + rem;
+            n = n/10; //    / 10 → removes last digit
+        }
+        return s;
+    } 
 }
