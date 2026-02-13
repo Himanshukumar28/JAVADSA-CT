46aab8be141d5d530fc35c1c4329d3df1f268582
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-02-13 23:12:17 +0530

    pat

--- Pattern.java
+++ Pattern.java
@@ -9,12 +9,12 @@ public class Pattern {
         // System.out.println();
         // }
 
-        // for(int i=1; i<=5; i++){
-        //     for(int j = 1; j<=i; j++){
-        //         System.out.print("#");
-        //     }
-        //     System.out.println();
-        // }
+        for(int i=1; i<=5; i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print("#");
+            }
+            System.out.println();
+        }
     
         int n = 5;
         for(int i= 1; i<=n; i++){
