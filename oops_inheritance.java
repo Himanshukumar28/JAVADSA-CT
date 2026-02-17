068e97247aa5c4d91d2b86c4b9522308465d4ba1
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-02-17 20:26:29 +0530

    multi

--- oops_inheritance.java
+++ oops_inheritance.java
@@ -3,17 +3,36 @@ public class oops_inheritance {
         child c1 = new child();
         c1.age = 6;
         c1.eyeColor = "Black";
-        
+
         System.out.println(c1.eyeColor);
     }
 }
+
+//SINGLE INHERITANCE
+
+// class parents {
+//     String name;
+//     int age;
+//     String gender;
+// }
+
+// class child extends parents {
+//     String eyeColor;
+// }
+
+//MULTILEVEL INHERITANCE
 
 class parents {
     String name;
     int age;
     String gender;
-
 }
+
 class child extends parents {
     String eyeColor;
 }
+
+class grandchild extends child{
+    String rollno;
+}
+
