25f23a3333e983b5d8c4b616858390f416814eaa
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-02-18 00:40:45 +0530

    heirarical

--- oops_inheritance.java
+++ oops_inheritance.java
@@ -1,14 +1,20 @@
 public class oops_inheritance {
     public static void main(String[] args) {
-        child c1 = new child();
-        c1.age = 6;
-        c1.eyeColor = "Black";
+        // child c1 = new child();
+        // c1.age = 6;
+        // c1.eyeColor = "Black";
 
-        grandchild c2 = new grandchild();
-        c2.roleno = 21;
+        // grandchild c2 = new grandchild();
+        // c2.roleno = 21;
 
-        System.out.println(c1.eyeColor);
-        System.out.println(c2.roleno);
+        // System.out.println(c1.eyeColor);
+        // System.out.println(c2.roleno);
+
+        child1 c1 = new child1();
+        c1.age = 18;
+        c1.height = 6;
+        System.out.println(c1.height);
+        System.out.println(c1.age);
     }
 }
 
@@ -26,17 +32,35 @@ public class oops_inheritance {
 
 //MULTILEVEL INHERITANCE
 
+// class parents {
+//     String name;
+//     int age;
+//     String gender;
+// }
+
+// class child extends parents {
+//     String eyeColor;
+// }
+
+// class grandchild extends child{
+//     int roleno;
+// }
+
+//HEIRARICAL INHERITANCE
+
 class parents {
     String name;
     int age;
     String gender;
 }
-
-class child extends parents {
-    String eyeColor;
+class child1  extends parents{
+    int height;
+}
+class child2 extends parents{
+    int height;
+}
+class child3 extends parents{
+    int height;
 }
 
-class grandchild extends child{
-    int roleno;
-}
 
