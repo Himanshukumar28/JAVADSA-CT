388eaf436142e6a71e4b480f831b39cb38766cb0
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-02-21 20:17:35 +0530

    poly

--- oops_Polymorphism.java
+++ oops_Polymorphism.java
@@ -1,15 +1,20 @@
+//pllymorphism --> polymer --> naam ek kaam anek
+    //Method/function overloading
+    //function name--> same
+    //function parameter--> different
+
+    //Method/function overriding
+    //function name--> same
+    //function parameter--> same
 public class oops_Polymorphism {
     public static void main(String[] args) {
         int a = 10 ;
         int b = 12;
-        int c = 13;
+        int c = 0; //dummy
         System.out.println(add(a, b));
         System.out.println(add(a, b, c));
     }
-    //pllymorphism --> polymer --> naam ek kaam anek
-    //Method/function overloading
-    //function name--> same
-    //function parameter--> different
+    
 
     public static int add(int a , int b){
         return a + b;
