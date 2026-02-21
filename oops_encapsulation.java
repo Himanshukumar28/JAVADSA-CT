7efe0a7bf1f5b62987b5dab93d88b8d0337e3af6
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-02-22 00:15:44 +0530

    settter

--- oops_encapsulation.java
+++ oops_encapsulation.java
@@ -1,19 +1,37 @@
 public class oops_encapsulation {
     public static void main(String[] args) {
-        Animal a1 = new Animal();
-        a1.name = "dogesh";
-        a1.age = 8;
-        System.out.println(a1.name);
+
+        Student s1 = new Student();
+       // s1.name = "Himanshu"; // Not possible because name entity is private
+        s1.setName("himanshu"); // setName change the value after calling the setName funnction
+        s1.setAge(20);
+        s1.setGender("Mail");
+        System.out.println(s1.getName()); //getName return the  value
+        System.out.println(s1.getAge());
+        System.out.println(s1.getGender());
     }
 }
-// class Animal{
-//     //variable
-//     String name;
-//     int age;
-//     //methode
-//     //properties
-
-//     public void Barking{
-//         System.out.println("Dog is Barking");
-//     }
-// }
+class Student{
+    private String name;
+    private int age;
+    private String gender;
+    //setter
+    public void setName(String name){       //Non static Kyunki access krni hai main function me ess student class ke function ko
+        this.name = name;
+    }
+    public String getName(){
+        return name;   
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setGender(String gender){
+        this.gender = gender;
+    }
+    public String getGender(){
+        return gender;
+    }
+}
