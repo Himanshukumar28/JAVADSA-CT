90f8f59804696cfce393706de508e41619ee7228
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-04-21 23:32:33 +0530

    Update Second_Largest.java
    new commit

--- ARRAY/Second_Largest.java
+++ ARRAY/Second_Largest.java
@@ -4,10 +4,10 @@ import java.util.*;
 public class Second_Largest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Array size: ");
+        System.out.print("Enter Array size:- ");
         int size = sc.nextInt();
 
-        System.out.print("Enter Array Element: ");
+        System.out.print("Enter Array Element:- ");
         int[] arr = new int[size];
         for(int i =0; i<size; i++){
             arr[i] = sc.nextInt();
