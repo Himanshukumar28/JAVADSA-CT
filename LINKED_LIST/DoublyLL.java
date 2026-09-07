e3b34ba8b7e0d715b67d5ffc1ae7cf2d56a7b408
Himanshu Kumar <himanshukashyapg01@gmail.com>
2026-09-07 15:59:49 +0530

    Complete doublyLL code

--- LINKED_LIST/DoublyLL.java
+++ LINKED_LIST/DoublyLL.java
@@ -89,6 +89,8 @@ public class DoublyLL {
     //====================
     //Traversal
     //====================
+    
+    //FORWARD
     public void printList(){
         Node temp = head;
         while(temp != null){
@@ -98,10 +100,147 @@ public class DoublyLL {
         System.out.println();
     }
 
+    //BACKWARD
+    public void printBackward(){
+        Node temp = tail;
+        while(temp != null){
+            System.out.print("<-"+ temp.data);
+            temp = temp.prev;
+        }
+        System.out.println();
+    }
+//==========================
+
+    //SEARCH AN ELEMENT IN LL
+    public boolean searchInLL(int target){
+        if(head == null){
+            System.out.println("NO NODE INSIDE LL");
+            return false;
+        }
+        Node temp = head;
+        while(temp != null){
+            if(temp.data == target){
+                return true;
+            }
+            else{
+                temp = temp.next;
+            }
+        }
+        return false;
+    }
+
+    //=======================
+    //DELETION
+    //=======================
+
+    //DELETE AT HEAD
+    public void deleteHead(){
+        if(head == null){
+            System.out.println("No Need to delete");
+            return ;
+        }
+        //single node
+        if(head == tail){
+            head = null;
+            tail = null;
+            //always remember
+            size = 0; //or size---
+            return ;
+        }
+        //LL has more than 1 node
+        head = head.next;
+        head.prev = null;
+        size--;
+    }
+
+    //DELETE AT TAIL.
+    public void deleteTail(){
+        if(head == null){
+            System.out.println("No Need to delete");
+            return ;
+        }
+        if(head == tail){
+            head = null;
+            tail = null;
+            size = 0;
+            return ;
+        }
+        //LL has more than 1 node
+        Node currNode = tail;
+        Node prevNode = tail.prev;
+
+        //link change
+        prevNode.next = null;
+        currNode.prev = null;
+        //tail update
+        tail = prevNode;
+        size--;
+    }
+
+    //DELETE AT POSITION
+    public void deletePosition(int position){
+        if(position < 1 || position > size+1){
+            System.out.println("Invalid Condition");
+            return ;
+        }
+        if(position == 1){
+            deleteHead();
+            return ;
+        }
+        if(position == size){
+            deleteTail();
+            return ;
+        }
+        //Main Logic
+
+        //firstly reach to that position
+        Node currNode = head;
+        for(int i = 1; i<=position-1; i++){
+            currNode = currNode.next;
+        }
+        //currNode is at the right place, i.e at the node which is to be deleted
+        Node prevNode = currNode.prev;
+        Node nextNode = currNode.next;
+
+        //change links
+        prevNode.next = nextNode;
+        nextNode.prev = prevNode;
+        currNode.prev = null;
+        currNode.next = null;
+        // size update
+        size--;
+    }
+
 
     public static void main(String[] args) {
-        DoublyLL mylist = new DoublyLL();
-        mylist.insertionAtHead(10);
-        mylist.printList();
+        DoublyLL myList = new DoublyLL();
+
+        myList.insertionAtHead(10);
+        myList.printList();
+
+        myList.insertionAtHead(12);
+        myList.printList();
+
+        myList.insertAtTail(20);
+        myList.printList();
+
+        myList.insertAtTail(60);
+        myList.printList();
+
+        myList.insertAtPosition(2, 80);
+        myList.printList();
+
+        myList.printBackward();
+
+        System.out.println("Found Or Not: " + myList.searchInLL(55));
+        
+        myList.deleteHead();
+        myList.printList();
+
+        // myList.deleteTail();
+        // myList.printList();
+
+        myList.deletePosition(2);
+        myList.printList();
     }
 }
